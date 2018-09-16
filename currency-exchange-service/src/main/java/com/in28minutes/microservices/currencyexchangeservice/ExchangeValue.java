b9977c22df1b1a34c42c94756df22f71b68cc30f
igorceridorio <igorceridorio@gmail.com>
2018-09-16 15:03:52 -0300

    New microservice upload: currency-exchange-service

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,34 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class ExchangeValue {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+
+	@Column(name = "currency_to")
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	
+	private int port;
+	
+}
