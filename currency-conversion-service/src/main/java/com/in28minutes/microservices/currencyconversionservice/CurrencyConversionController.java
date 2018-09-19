0565f42ff4584b091520ef3e6076c51276a541ab
Igor Ceridorio <igor.ferreira@opus-software.com.br>
2018-09-19 18:59:21 -0300

    Adding Feign to the currency-conversion-service

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 
 	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(
@@ -31,6 +35,27 @@ public class CurrencyConversionController {
 		
 		// obtain the value from the response
 		CurrencyConversionBean response = responseEntity.getBody();
+		
+		return CurrencyConversionBean
+				.builder()
+				.id(response.getId())
+				.from(from)
+				.to(to)
+				.conversionMultiple(response.getConversionMultiple())
+				.quantity(quantity)
+				.totalCalculatedAmount(quantity.multiply(response.getConversionMultiple()))
+				.port(response.getPort())
+				.build();
+	}
+	
+	@GetMapping("currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		// obtain the value from the response
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		
 		return CurrencyConversionBean
 				.builder()
